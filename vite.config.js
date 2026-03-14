648d36a1163117d9fc893e18717141e17d9cddad
LiliaVirt <liliia.virt-kushnir.pp.2023@lpnu.ua>
2026-03-14 21:00:46 +0200

    feat: add build automation, ESLint config and env variables

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    exclude: ['**/node_modules/**', '**/e2e/**'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+    }
+  }
+})
